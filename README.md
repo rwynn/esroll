251153f0527814593a5c0027534b6d0431ab29f2
Ryan Wynn <ryan.m.wynn@gmail.com>
2016-06-03 02:23:52 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,170 @@
 # esroll
 a go daemon to ensure some elasticsearch best practices
+
+<img width="480" height="339" src="https://raw.github.com/rwynn/esroll/master/images/esroll.jpg"/>
+
+### Install ###
+
+	go get github.com/rwynn/esroll
+
+### Design ###
+
+esroll is a go daemon to ensure some elasticsearch scaling best practices such as
+[Index per Time Frame](https://www.elastic.co/guide/en/elasticsearch/guide/current/time-based.html) and
+[Retiring Data](https://www.elastic.co/guide/en/elasticsearch/guide/current/retiring-data.html).
+
+esroll helps you manage time based indices by keeping a pair of aliases (one for indexing and one for search)
+pointing to a set of indices it creates periodically. At a minimum you need to configure esroll to have an `indexTarget`
+and a `rollUnit`.  
+
+Say, for example, that you configure esroll with an indexTarget of `logs`, a rollUnit of `years`, and set searchAliases 
+to `2`. esroll would begin by checking the existence of an index `logs_2016`.  If the index does not exist, esroll would 
+create it and assign the aliases `logs` and `logs_search`. The former alias is the one meant for indexing new data
+and the latter is meant for searching data. This gives you the following index:
+
+	logs_2016 -> aliases logs and log_search
+
+In daemon mode esroll will continue running and wait until the year rolls over to 2017. At the point esroll would
+do the following:
+
+Check for the existence of an index `logs_2017`.  If the index does not exist, esroll will create it and then
+assign the aliases `logs` and `logs_search`.  Since there should only ever be one real index backing the `logs`
+alias (the most recent), esroll will remove the `logs` alias from the `logs_2016` index.  Since the `searchAliases` 
+option is set to 2 and there are equally 2 time based indices at this point, esroll will keep the `logs_search` 
+alias on the `logs_2016` index.
+
+At that point you would have the following indices:
+
+	logs_2017 -> aliases logs and logs_search
+	logs_2016 -> alias logs_search
+
+When you index into `logs` you are actually indexing into the time based index for 2017.  And when you
+query `logs_search` you are actually searching 2 time based indices - one for 2016 and one for 2017.
+
+When an index goes from having both aliases `logs` and `logs_search` to having only the search alias `logs_search`,
+you can tell esroll to optimize the index by using the `optimizeOnRoll` setting.  When indices go from indexing and
+searching to only searching (readonly) you can get peformance gains on search by doing an optimization to reduce the segments
+within the index. The max number of segments to optimize to is also available as a setting.
+
+This rolling over algorithm would continue happening each time the year changes. In this example, when the year
+turns 2018, you would have the following indices:
+
+	logs_2018 -> aliases logs and logs_search
+	logs_2017 -> alias logs_search
+	logs_2016 -> alias logs_search
+
+At this point some other esroll settings come into play.  Since the setting searchAliases was set to 2, and the
+alias `logs_search` currently points to 3 indices, esroll needs to fix this.  It will remove the alias
+`logs_search` from `logs_2016`.  Thus, you get the following:
+
+	logs_2018 -> aliases logs and logs_search
+	logs_2017 -> alias logs_search
+	logs_2016 
+
+Esroll lets you configure what you want to do with indices which no longer have an alias.  You can configure
+esroll to delete them, close them, or just keep them open. 
+
+In this example a `rollUnit` of years was used to keep things simple.  But rolling over once a year is probably not
+optimal if you have alot of data coming into elasticsearch.  esroll provides the following values for `rollUnit` - minutes,
+hours, days, months, and years.  esroll provides another option `rollIncrement` which is an integer. Together `rollUnit`
+and `rollIncrement` allow you to tell esroll to run its algorithm at intervals like 20 minutes, 3 hours, or 5 months.
+
+When something like 3 hours is used, its important to understand that esroll does not necessary roll 3 hours from the last
+roll but rather when the hour of the day % 3 == 0.  It's possible that if you start esroll with this setting 
+at 1am, esroll would do an initial roll at 1am and then again at 3am, 6am, 9am, etc.  So even though you had set to roll
+every 3 hours, due to index initialization, there is only 2 hours between the 1st and 2nd roll.  
+
+From an elasticsearch client perspective you would usually deal only with the pair of indexes aliases created by
+esroll and not the time based raw indexes.  This allows your client code concern itself with logical index names (indexing
+and searching) even though the indexes backing those aliases are changing over time.  
+
+Finally, the way the algorithm is explained above it may lead you to think that alias updates at the time of a roll are done
+serially, however, this is not the case.  All the alias updates are gathered together on the roll and made in one 
+request to elasticsearch.  
+
+### Usage ###
+
+Before running esroll you will probably want to configure it. It's not actually required that you config esroll before
+running it though.  That's because the esroll configuration is stored in elasticsearch and esroll will poll
+periodically for changes in its configuration.
+
+Configuring esroll is done by indexing documents into the `esroll` index with the type `config`.  The following is an
+example of how to get a configuration into elasticsearch...
+
+	curl -XPUT localhost:9200/esroll/config/snowball -d '{
+		"targetIndex": "snowball",
+		"rollUnit": "minutes",
+		"rollIncrement": 3,
+		"searchAliases": 4,
+		"searchSuffix": "search",
+		"deleteOld": false,
+		"closeOld": true,
+		"optimizeOnRoll": true,
+		"optimizeMaxSegments": 2,
+		"settings": {
+			"index.routing.allocation.include.box_type" : "strong",
+			"index": {
+				"number_of_replicas": 5
+			}
+		},
+		"settingsOnRoll": {
+			"index.routing.allocation.include.box_type" : "medium"
+		}
+	}'
+
+The example above creates a document describing one rolling index that esroll will manage. The most important parts of
+this configuration are the `targetIndex` and `rollUnit`.  The `targetIndex` is not actually required, though.  If you
+omit it, the `targetIndex` will default to the ID of the configuration document.  So in this case `targetIndex` is
+redundant since the ID of the configuration document is the same.  The `rollUnit` setting tells esroll the time unit
+for which you want to roll.  The `rollIncrement` setting is combined with the `rollUnit` setting to further detail
+when you want the roll to occur.  If `rollIncrement` is not supplied it defaults to `1`.  
+
+So above we are telling esroll to run its roll algorithm every time clock seconds == 0 and clock minute % 3 == 0.  If
+we ran esroll at 1:00PM GMT it would immediatly run it's roll algorithm and create the index `snowball_2016-05-31-13-00`
+and give that index aliases `snowball` and `snowball_search`.  Then at 1:03PM GMT, 1:06PM GMT, and so on, it would
+perform a roll - create a new timestamped index and adjust the aliases.
+
+Let's look at some of the other settings we have configured...
+
+	searchAliases = 4 -- keep up to 4 recent indexes with the search alias
+	searchSuffix = search -- suffix the search alias names with 'search'
+	deleteOld = false -- do not delete indices when the alias count for the index drops to 0
+	closeOld = true -- flush and close indices when the alias count for the index drops to 0
+	optimizeOnRoll = true -- optimize the index when the alias count for the index drops to 1 (search)
+	optimizeMaxSegments = 2 -- pass max_num_segments=2 when optimizing
+	settings = elasticsearch settings -- use the specificed settings when creating each new time based index
+	settingsOnRoll = elasticseach settings -- update the index settings with this when the alias count for the index drops to 1 (search)
+
+You may have as many of these configuration documents as you would like.  esroll will find them (even when running)
+and start using them.
+
+`rollUnit` is the only configuration option which is required.  the defaults for missing options are:
+
+	targetIndex          : the ID of the configuration document
+	rollIncrement        : 1
+	searchAliases        : 2
+	searchSuffix         : "search"
+	deleteOld            : false
+	closeOld             : false
+	optimizeOnRoll       : false
+	optimizeMaxSegments  : do not specify max segments when optimizing
+	settings	     : do not specify settings when creating indexes
+	settingsOnRoll       : do not update settings on roll
+
+You start esroll as follows:
+
+	esroll \[-url ES-REST-URL\] \[-daemon true|false\] \[-pem PATH_TO_PEM_FILE\]
+
+esroll can be run in 2 modes: single shot and daemon mode.  The single shot mode is the default.  In that mode esroll
+will pull down all the settings documents, run a single roll on each of them and quit.  Use this mode if you wish to
+externally control when rolls are peformed (such as via a cron job).  The daemon mode is enabled by supplying `-daemon`
+on the command line.  In this mode esroll will manage scheduling internally.  It will run until it is explicitly stopped
+and periodically roll indexes according to the settings, updating its configuration dynamically along the way by pulling
+the most recent versions of the configuration documents.
+
+By default esroll will expect the elasticsearch REST API to be available at http://localhost:9200.  If you need to change
+this supply the `-url` argument and specify the URL to the elasticsearch REST API.
+
+If you need to install a self-signed certificate for connections to the elasticsearch REST API you can do so using 
+the `-pem` argument with the path to your PEM file.
+
---
+++ esroll.go
@@ -0,0 +1,391 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	elastigo "github.com/mattbaird/elastigo/lib"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"sort"
+	"strings"
+	"syscall"
+	"time"
+)
+
+var rollUnits = map[string]bool{
+	"minutes": true,
+	"hours":   true,
+	"days":    true,
+	"months":  true,
+	"years":   true,
+}
+
+const ExampleConfig string = `curl -XPUT localhost:9200/esroll/config/snowball -d '{
+	"targetIndex": "snowball",
+	"rollUnit": "minutes",
+	"rollIncrement": 3,
+	"searchAliases": 4,
+	"searchSuffix": "search",
+	"deleteOld": false,
+	"closeOld": true,
+	"optimizeOnRoll": true,
+	"optimizeMaxSegments": 2,
+	"settings": {
+		"index": {
+			"number_of_replicas": 5
+		}
+	}
+}'`
+
+type Configs []IndexConfig
+type ByIndexAge []Index
+
+func (a ByIndexAge) Len() int           { return len(a) }
+func (a ByIndexAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByIndexAge) Less(i, j int) bool { return a[i].Name < a[j].Name }
+
+type EsRollConfig struct {
+	ElasticUrl     string
+	ElasticPemFile string
+	Daemon         bool
+}
+
+type Index struct {
+	Name   string
+	Status string
+}
+
+type IndexConfig struct {
+	Id                      string
+	Settings                map[string]interface{} `json:"settings",omitempty`
+	SettingsOnRoll          map[string]interface{} `json:"settingsOnRoll",omitempty`
+	TargetIndex             string                 `json:"targetIndex"`
+	RollIncrement           int                    `json:"rollIncrement"`
+	RollUnit                string                 `json:"rollUnit"`
+	IndexesToAliasForSearch int                    `json:"searchAliases",omitempty`
+	SearchSuffix            string                 `json:"searchSuffix",omitempty`
+	DeleteOldIndexes        bool                   `json:"deleteOld",omitempty`
+	CloseOldIndexes         bool                   `json:"closeOld",omitempty`
+	OptimizeOnRoll          bool                   `json:"optimizeOnRoll",omitempty`
+	OptimizeMaxSegments     int                    `json:"optimizeMaxSegments",omitempty`
+}
+
+func GetConfigs(conn *elastigo.Conn) (Configs, error) {
+	res, err := elastigo.Search("esroll").Type("config").Result(conn)
+	if err != nil {
+		return nil, err
+	}
+	if res.Hits.Total == 0 {
+		return nil, errors.New("configuration documents not found")
+	}
+	var configs Configs
+	for _, hit := range res.Hits.Hits {
+		var configData IndexConfig
+		if err := json.Unmarshal(*hit.Source, &configData); err != nil {
+			return nil, err
+		}
+		configData.Id = hit.Id
+		configData.SetDefaults()
+		if err := configData.Validate(); err != nil {
+			fmt.Println(err)
+		} else {
+			configs = append(configs, configData)
+		}
+	}
+	return configs, nil
+}
+
+func PutSettings(conn *elastigo.Conn, index string, settings map[string]interface{}) error {
+	url := fmt.Sprintf("/%s/_settings", index)
+	body, err := json.Marshal(settings)
+	if err != nil {
+		return err
+	}
+	_, err = conn.DoCommand("PUT", url, nil, body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateAliases(conn *elastigo.Conn, body string) error {
+	_, err := conn.DoCommand("POST", "/_aliases", nil, body)
+	return err
+}
+
+func UpdateAliasAction(action string, index string, alias string) string {
+	return fmt.Sprintf(`{ "%s" : { "index" : "%s", "alias" : "%s" } }`, action, index, alias)
+}
+
+func UpdateAliasActions(actions []string) string {
+	var request bytes.Buffer
+	request.WriteString(`{ "actions": [ `)
+	request.WriteString(strings.Join(actions, ", "))
+	request.WriteString(` ] }`)
+	return request.String()
+}
+
+func (config *IndexConfig) Validate() error {
+	if config.Id == "" {
+		return errors.New("configuration id is missing")
+	}
+	if config.IndexesToAliasForSearch < 0 {
+		return errors.New("searchIndexes must be greater than or equal to 0")
+	}
+	if config.RollUnit == "" {
+		return errors.New("configuration id[" + config.Id + "] is invalid. rollUnit is required.")
+	} else if rollUnits[config.RollUnit] == false {
+		return errors.New("configuration id[" + config.Id + "] is invalid. rollUnit must be one of minutes, hours, days, months, or years.")
+	}
+	if config.OptimizeMaxSegments < 0 {
+		return errors.New("optimizeMaxSegments must be greater than or equal to 0")
+	}
+	return nil
+}
+
+func (config *IndexConfig) SetDefaults() {
+	if config.TargetIndex == "" {
+		config.TargetIndex = config.Id
+	}
+	if config.SearchSuffix == "" {
+		config.SearchSuffix = "search"
+	}
+	if config.IndexesToAliasForSearch == 0 {
+		config.IndexesToAliasForSearch = 2
+	}
+}
+
+func (config *IndexConfig) IndexSuffixFormat() string {
+	if config.RollUnit == "minutes" {
+		return "2006-01-02-15-04"
+	} else if config.RollUnit == "hours" {
+		return "2006-01-02-15"
+	} else if config.RollUnit == "days" {
+		return "2006-01-02"
+	} else if config.RollUnit == "months" {
+		return "2006-01"
+	} else {
+		return "2006"
+	}
+}
+
+func (config *IndexConfig) NextIndex(t time.Time) string {
+	suffix := t.Format(config.IndexSuffixFormat())
+	return config.TargetIndex + "_" + suffix
+}
+
+func (config *IndexConfig) Roll(conn *elastigo.Conn, t time.Time) error {
+	nextIndex := config.NextIndex(t)
+	if exists, _ := conn.ExistsIndex(nextIndex, "", nil); exists {
+		return nil
+	}
+	settings := make(map[string]interface{})
+	if config.Settings != nil {
+		settings = config.Settings
+	}
+	oldIndexes := config.OldIndexNames(conn)
+	if _, err := conn.CreateIndexWithSettings(nextIndex, settings); err != nil {
+		return err
+	}
+	var cleanup, actions, optimizes []string
+	var searchSuffix string = "_" + config.SearchSuffix
+	actions = append(actions, UpdateAliasAction("add", nextIndex, config.TargetIndex))
+	actions = append(actions, UpdateAliasAction("add", nextIndex, config.TargetIndex+searchSuffix))
+	searchIndexes := 1 + len(oldIndexes)
+	for i, oldIndex := range oldIndexes {
+		actions = append(actions, UpdateAliasAction("remove", oldIndex.Name, config.TargetIndex))
+		retire := (searchIndexes - 1) >= config.IndexesToAliasForSearch
+		if retire {
+			actions = append(actions, UpdateAliasAction("remove", oldIndex.Name, config.TargetIndex+searchSuffix))
+			searchIndexes = searchIndexes - 1
+			if config.DeleteOldIndexes {
+				cleanup = append(cleanup, oldIndex.Name)
+			} else if config.CloseOldIndexes && oldIndex.Status == "open" {
+				cleanup = append(cleanup, oldIndex.Name)
+			}
+		} else if config.OptimizeOnRoll || (config.SettingsOnRoll != nil) {
+			if i == (len(oldIndexes) - 1) {
+				optimizes = append(optimizes, oldIndex.Name)
+			}
+		}
+	}
+	if err := UpdateAliases(conn, UpdateAliasActions(actions)); err != nil {
+		return err
+	}
+	if len(cleanup) > 0 {
+		if config.DeleteOldIndexes {
+			clean := strings.Join(cleanup, ",")
+			if _, err := conn.DeleteIndex(clean); err != nil {
+				return err
+			}
+		} else if config.CloseOldIndexes {
+			clean := strings.Join(cleanup, ",")
+			if _, err := conn.Flush(clean); err != nil {
+				return err
+			}
+			if _, err := conn.CloseIndex(clean); err != nil {
+				return err
+			}
+		}
+	}
+	if len(optimizes) > 0 {
+		if config.SettingsOnRoll != nil {
+			optimize := strings.Join(optimizes, ",")
+			if err := PutSettings(conn, optimize, config.SettingsOnRoll); err != nil {
+				return err
+			}
+		}
+		if config.OptimizeOnRoll {
+			args := make(map[string]interface{})
+			if config.OptimizeMaxSegments != 0 {
+				args["max_num_segments"] = config.OptimizeMaxSegments
+			}
+			if _, err := conn.OptimizeIndices(args, optimizes...); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (config *IndexConfig) ShouldRoll(t time.Time) bool {
+	var roll bool = false
+	if config.RollUnit == "minutes" {
+		roll = t.Second() == 0
+		if config.RollIncrement != 0 {
+			roll = roll && t.Minute()%config.RollIncrement == 0
+		}
+	} else if config.RollUnit == "hours" {
+		roll = t.Second() == 0 && t.Minute() == 0
+		if config.RollIncrement != 0 {
+			roll = roll && t.Hour()%config.RollIncrement == 0
+		}
+	} else {
+		roll = t.Second() == 0 && t.Minute() == 0 && t.Hour() == 0
+		if config.RollIncrement != 0 {
+			if config.RollUnit == "days" {
+				roll = roll && t.YearDay()%config.RollIncrement == 0
+			} else if config.RollUnit == "months" {
+				roll = roll && int(t.Month())%config.RollIncrement == 0
+			} else if config.RollUnit == "years" {
+				roll = roll && t.Year()%config.RollIncrement == 0
+			}
+		}
+	}
+	return roll
+}
+
+func (config *IndexConfig) OldIndexNames(conn *elastigo.Conn) []Index {
+	var indexes []Index
+	pattern := config.TargetIndex + "_*"
+	catIndexInfo := conn.GetCatIndexInfo(pattern)
+	for _, info := range catIndexInfo {
+		indexes = append(indexes, Index{Name: info.Name, Status: info.Status})
+	}
+	sort.Sort(ByIndexAge(indexes))
+	return indexes
+}
+
+func AddWork(t time.Time) bool {
+	return t.Second() == 0
+}
+
+func (config *EsRollConfig) ConfigTransport() error {
+	if config.ElasticPemFile != "" {
+		certs := x509.NewCertPool()
+		if ca, err := ioutil.ReadFile(config.ElasticPemFile); err == nil {
+			certs.AppendCertsFromPEM(ca)
+		} else {
+			return err
+		}
+		tlsConfig := &tls.Config{RootCAs: certs}
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = tlsConfig
+	}
+	return nil
+}
+
+func main() {
+	var mainConfig EsRollConfig
+	flag.StringVar(&mainConfig.ElasticUrl, "url", "", "ElasticSearch connection URL")
+	flag.StringVar(&mainConfig.ElasticPemFile, "pem", "", "Path to a PEM file for secure connections to ElasticSearch")
+	flag.BoolVar(&mainConfig.Daemon, "daemon", false, "Run as a daemon")
+	flag.Parse()
+	if err := mainConfig.ConfigTransport(); err != nil {
+		panic(err)
+	}
+	conn := elastigo.NewConn()
+	if mainConfig.ElasticUrl != "" {
+		conn.SetFromUrl(mainConfig.ElasticUrl)
+	}
+	configs, err := GetConfigs(conn)
+	if err != nil {
+		if mainConfig.Daemon {
+			fmt.Println("Configuration for esroll invalid or not found, waiting till one exists")
+		} else {
+			fmt.Println("Configuration for esroll invalid or not found")
+		}
+		fmt.Println("You can create one with ...")
+		fmt.Println(ExampleConfig)
+		if !mainConfig.Daemon {
+			os.Exit(1)
+		}
+	}
+	if mainConfig.Daemon {
+		var configTicker = time.NewTicker(10 * time.Second)
+		var workQ = make(chan (time.Time))
+		var initQ = make(chan (IndexConfig))
+		go func(conn *elastigo.Conn) {
+			sigs := make(chan os.Signal, 1)
+			signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+			<-sigs
+			configTicker.Stop()
+			conn.Close()
+			os.Exit(0)
+		}(conn)
+		go func() {
+			var clock = time.NewTicker(1 * time.Second)
+			for {
+				t := <-clock.C
+				if AddWork(t) {
+					workQ <- t
+				}
+			}
+
+		}()
+		for {
+			select {
+			case t := <-workQ:
+				for _, conf := range configs {
+					if conf.ShouldRoll(t) {
+						conf.Roll(conn, t)
+					}
+				}
+			case conf := <-initQ:
+				conf.Roll(conn, time.Now().UTC())
+			case <-configTicker.C:
+				configs, err = GetConfigs(conn)
+				if err == nil {
+					for _, config := range configs {
+						exists, _ := conn.ExistsIndex(config.TargetIndex, "", nil)
+						if !exists {
+							go func() {
+								initQ <- config
+							}()
+						}
+					}
+				}
+			}
+		}
+	} else {
+		for _, conf := range configs {
+			conf.Roll(conn, time.Now().UTC())
+		}
+	}
+}
